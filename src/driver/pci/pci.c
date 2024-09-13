cc95420d9ddad141ca030dddf4918406d7a9382f
Serpent03 <dumbpilot03@gmail.com>
2024-09-13 14:27:20 +0530

    refactor

--- src/driver/pci/pci.c
+++ src/driver/pci/pci.c
@@ -1,12 +1,15 @@
 #include "../../headers/driver/pci.h"
 #include "../../headers/sys/hal.h"
 
+void read_pci_header(uint32 *address) {
+  /* read the config address(PCI_CONFIG_ADDR_REG) which contains
+  details on vendor, device and other miscellaneous items. */
+}
+
 void install_pci() {
   /* configure the PCI bus. One of the first things that need to be
   done is to get ATA/IDE port data. */
 }
-
-
 
 void init_pci() {
   install_pci();
