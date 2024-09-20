21c1db4c1298dfacdfe383a72e118c45182c4242
Serpent03 <dumbpilot03@gmail.com>
2024-09-20 10:45:50 +0530

    feat: DWORD read/write through HAL

--- src/headers/sys/hal.h
+++ src/headers/sys/hal.h
@@ -5,14 +5,20 @@
 /* Read data(u8) from a port P. */
 uint8 port_byte_read(uint16 P);
 
-/* Write data N to a port P. */
+/* Write data N(u8) to a port P. */
 void port_byte_write(uint16 P, uint8 N);
 
 /* Read data(u16) from a port P. */
 uint16 port_word_read(uint16 P);
 
-/* Write data N to a port P. */
+/* Write data N(u16)to a port P. */
 void port_word_write(uint16 P, uint16 N);
+
+/* Read data(u32) from a port P. */
+uint32 port_dword_read(uint16 P);
+
+/* Write data N(u32) to a port P. */
+void port_dword_write(uint32 P, uint16 N);
 
 /* A simple and imprecise delay inductor. Use as less as possible. */
 void io_wait();
--- src/kernel/hal.c
+++ src/kernel/hal.c
@@ -16,8 +16,18 @@ uint16 port_word_read(uint16 P) {
   return result;
 }
 
+uint32 port_dword_read(uint16 P) {
+  uint32 result;
+  __asm__("in %%dx, %%eax" : "=a"(result) : "d"(P));
+  return result;
+}
+
 void port_word_write(uint16 P, uint16 N) {
   __asm__("out %%ax, %%dx" ::"a"(N), "d"(P));
+}
+
+void port_dword_write(uint16 P, uint32 N) {
+  __asm__("out %%eax, %%dx" ::"a"(N), "d"(P));
 }
 
 void io_wait() { port_byte_write(0x80, 0); }
