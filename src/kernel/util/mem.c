7c1a4910028e5a5b5c973f1e18fef59b48ff50e3
Serpent03 <dumbpilot03@gmail.com>
2025-04-06 17:52:11 +0530

    minor: buffered memcpy

--- src/kernel/util/mem.c
+++ src/kernel/util/mem.c
@@ -8,9 +8,14 @@ void memset(uint8 *src, uint8 d, uint32 n) {
 
 /* Copy N bytes of SRC to DST. */
 void memcpy(uint8 *dst, uint8 *src, uint32 n) {
-  while (n--) {
-    dst[n] = src[n];
-  }
+  uint32 m = n;
+  uint8 buf[n];
+  /* This should ensure that we don't end up
+  having trouble where the address are close-by. */
+  while (m--)
+    buf[n] = src[m];
+  while (n--)
+    dst[n] = buf[n];
 }
 
 /* Set N words of SRC to D. */
@@ -22,7 +27,11 @@ void memsetw(uint16 *src, uint16 d, uint32 n) {
 
 /* Copy N words of SRC to DST. */
 void memcpyw(uint16 *dst, uint16 *src, uint32 n) {
-  while (n--) {
-    dst[n] = src[n];
-  }
+  uint32 m = n;
+  uint16 buf[n];
+  while (m--)
+    buf[m] = src[m];
+  while (n--) 
+    dst[n] = buf[n];
+  
 }
